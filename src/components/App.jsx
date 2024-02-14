b1eb4dbe3f2ff4bf74b2b291f1e27681b5444cab
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-14 12:49:49 +0300

    Capture bill input change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
-import Bill from './BillInput';
+import BillInput from './BillInput';
 import Rating from './Rating';
 import Output from './Output';
 import Reset from './Reset';
+import { useState } from 'react';
 
 const App = () => {
+  const [bill, setBill] = useState('');
+
   return (
     <div>
-      <Bill />
+      <BillInput bill={bill} onSetBill={setBill} />
       <Rating>How did you like the service?</Rating>
       <Rating>How did your friend like the service?</Rating>
       <Output />
--- src/components/BillInput.jsx
+++ src/components/BillInput.jsx
@@ -1,8 +1,13 @@
-const BillInput = () => {
+const BillInput = ({ bill, onSetBill }) => {
   return (
     <p>
       How much was the bill?
-      <input type="text" placeholder="Bill value" />
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
     </p>
   );
 };
