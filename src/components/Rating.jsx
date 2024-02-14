8d6c6c0531d67405db4dfc8bba0090fa4b0dccc3
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-14 12:30:28 +0300

    Add values to select fields

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,10 +3,10 @@ const Rating = ({ children }) => {
     <p>
       {children}
       <select>
-        <option>Dissatisfied 0%</option>
-        <option>It was okay 5%</option>
-        <option>It was good 10%</option>
-        <option>Absolutely amazing 10%</option>
+        <option value="0">Dissatisfied 0%</option>
+        <option value="5">It was okay 5%</option>
+        <option value="10">It was good 10%</option>
+        <option value="20">Absolutely amazing 10%</option>
       </select>
     </p>
   );
